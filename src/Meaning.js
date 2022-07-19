05231e523f875ef820f233001edee47931854f40
MariaASokolova <51476700+MariaASokolova@users.noreply.github.com>
2022-07-19 22:49:05 +0200

    added synonyms output

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,10 +8,16 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            <div>
+              <strong>Definition: </strong>
               {definition.definition}
+              <br />
+              <strong>Example: </strong>
               {definition.example}
-            </p>
+              <br />
+
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
           </div>
         );
       })}
