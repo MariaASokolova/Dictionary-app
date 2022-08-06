05b2fa42cf04f90928e78e8052647e9d66bb43e1
MariaASokolova <51476700+MariaASokolova@users.noreply.github.com>
2022-08-06 14:18:50 +0200

    added visual illustraions of searched words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,34 @@ import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search(event) {
     event.preventDefault();
-    //alert(`Searching for ${keyword}`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001d434677ea3724accb07dd8db3f773ecc";
+
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios
+      .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiKey}` } })
+      .then(handlePexelsResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -31,6 +44,9 @@ export default function Dictionary() {
         </form>
         <div className="hint">suggested words: wine, sand, sun...</div>
       </section>
+
+      <Photos photos={photos} />
+
       <Results results={results} />
     </div>
   );
